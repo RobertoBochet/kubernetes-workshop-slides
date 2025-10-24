55bfc32d9e87fcb6618e53d3555fd6f13ad18695
Roberto Bochet <r@robertobochet.me>
2025-10-24 22:50:32 +0200

    Add outro layout

--- layouts/outro.vue
+++ layouts/outro.vue
@@ -1,27 +1,46 @@
 <script setup lang="ts">
-import Center from "@slidev/client/layouts/center.vue"
-import { computed } from "vue"
-import { Icon } from "@iconify/vue"
-import { getSourceCode } from "../components/utilities/source_code"
-import { getAuthors, getTitle } from "../components/utilities/metadata"
+import Center from "@slidev/client/layouts/center.vue";
+import { computed } from "vue";
+import { Icon } from "@iconify/vue";
 
 const props = defineProps({
+  title: {
+    type: String,
+  },
   image: {
-    type: String
+    type: String,
   },
   backgroundSize: {
     type: String,
-    default: "cover"
-  }
-})
+    default: "cover",
+  },
+});
 
-const title = computed(() => getTitle($slidev))
-const authors = computed(() => getAuthors($slidev))
-const sourceCode = computed(() => getSourceCode($slidev))
+const title = computed(() => props.title ?? $slidev.configs.title);
+
+const authors = computed<{ name: string; email?: string }[]>(() =>
+  $slidev.configs.author
+    ?.split(",")
+    .map((i: string) => i.trim())
+    .map((i: string) => i.match(/^([^<>]+[^ ]) ?(?:<(.*)>)?$/))
+    .map((i: string[]) => ({ name: i[1], email: i[2] })),
+);
+
+const sourceCodeProvider = computed(() => {
+  const sourceCode = $slidev.configs.sourceCode;
+  if (!sourceCode) return null;
+  if (/(https?:\/\/)?github/.test(sourceCode)) return "github";
+  if (/(https?:\/\/)?gitlab/.test(sourceCode)) return "gitlab";
+  return "generic";
+});
 </script>
 
 <template>
-  <Center class="flex flex-col justify-around text-center" :image="props.image" :backgroundSize="props.backgroundSize">
+  <Center
+    class="flex flex-col justify-around text-center"
+    :image="props.image"
+    :backgroundSize="props.backgroundSize"
+  >
     <div class="text-20">Thank you!</div>
 
     <h1 class="mt-10">{{ title }}</h1>
@@ -35,12 +54,22 @@ const sourceCode = computed(() => getSourceCode($slidev))
       </p>
     </div>
 
-    <div class="abs-bl m-5 text-xl flex flex-row flex-nowrap items-center">
-      <LicenseBadge hide-text />
-      <a v-if="sourceCode" :href="sourceCode.url" target="_blank" class="slidev-icon-btn font-size-8 block">
-        <Icon icon="carbon:logo-github" v-if="sourceCode.provider === 'github'" />
-        <Icon icon="carbon:logo-gitlab" v-if="sourceCode.provider === 'gitlab'" />
-        <Icon icon="carbon:logo-git" v-if="sourceCode.provider === 'generic'" />
+    <div class="m-2 text-xl">
+      <a
+        v-if="$slidev.configs.sourceCode"
+        :href="$slidev.configs.sourceCode"
+        target="_blank"
+        class="slidev-icon-btn font-size-8"
+      >
+        <Icon
+          icon="carbon:logo-github"
+          v-if="sourceCodeProvider === 'github'"
+        />
+        <Icon
+          icon="carbon:logo-gitlab"
+          v-if="sourceCodeProvider === 'gitlab'"
+        />
+        <Icon icon="carbon:logo-git" v-if="sourceCodeProvider === 'generic'" />
       </a>
     </div>
 
