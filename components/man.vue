f4b3323d7ac86b11dd03739b6bb8a068a5765590
Roberto Bochet <r@robertobochet.me>
2025-10-24 23:22:58 +0200

    Create man component

---
+++ components/man.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  command: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="my-5 shiki shiki-themes vitesse-dark vitesse-light slidev-code">
+    <code class="language-shell">$ man {{ props.command }}</code>
+    <div class="pl-4 pt-1">
+      <span>DESCRIPTION</span><br />
+      <div class="pl-12"><slot></slot></div>
+    </div>
+  </div>
+</template>
