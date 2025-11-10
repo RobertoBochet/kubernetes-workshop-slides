50beff714031685ddd992030f24d3825350ccf4f
Roberto Bochet <r@robertobochet.me>
2025-11-10 03:00:32 +0100

    Add component v-space

---
+++ components/v-space.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed, PropType } from "vue"
+
+const sizes = {
+  sm: "1rem",
+  md: "2rem",
+  xl: "4rem",
+  xxl: "6rem"
+} as const
+
+const props = defineProps({
+  size: {
+    type: String as PropType<keyof typeof sizes>,
+    default: "sm"
+  }
+})
+
+const style = computed(() => ({ height: sizes[props.size] }))
+</script>
+
+<template>
+  <div :style="style" />
+</template>
