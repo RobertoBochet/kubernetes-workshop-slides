ca7fe2830c62b9eff1bebfd13e5899fa8f77b573
Roberto Bochet <r@robertobochet.me>
2026-04-07 18:56:48 +0200

    Add `two-cols-grid` template

---
+++ layouts/two-cols-grid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="slidev-layout two-cols-grid w-full h-full">
+    <div class="col-header">
+      <slot />
+    </div>
+    <div class="grid grid-cols-2 gap-x-1 gap-y-1 mt-10">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.two-cols-grid :deep(p) {
+  margin: 0;
+}
+
+.two-cols-grid :deep(h2),
+.two-cols-grid :deep(h3) {
+  margin-bottom: 0;
+}
+</style>
