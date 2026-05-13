4d6c4aed6473ef3aea0f58a9224024258b4654b5
Roberto Bochet <r@robertobochet.me>
2026-05-13 03:46:11 +0200

    Create Label component

---
+++ components/label.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { computed, PropType } from "vue"
+
+type LabelColor = "blue" | "orange" | "red" | "green" | "gray"
+
+const { color } = defineProps({
+  color: {
+    type: String as PropType<LabelColor>,
+    default: "gray"
+  }
+})
+
+const colorClass = computed(() => `label--${color}`)
+</script>
+
+<template>
+  <span :class="['label', colorClass]">
+    <slot></slot>
+  </span>
+</template>
+
+<style scoped>
+.label {
+  display: inline-block;
+  padding: 0 4px;
+  border-radius: 3px;
+  font-size: 0.9em;
+  font-weight: 600;
+  vertical-align: baseline;
+  margin: 0 2px;
+  line-height: 1.5;
+  border: none;
+  color: inherit;
+
+  &--blue {
+    background-color: light-dark(theme("colors.blue.500"), theme("colors.blue.600"));
+  }
+  &--orange {
+    background-color: light-dark(theme("colors.orange.500"), theme("colors.orange.600"));
+  }
+  &--red {
+    background-color: light-dark(theme("colors.red.500"), theme("colors.red.600"));
+  }
+  &--green {
+    background-color: light-dark(theme("colors.green.500"), theme("colors.green.600"));
+  }
+  &--gray {
+    background-color: light-dark(theme("colors.gray.500"), theme("colors.gray.600"));
+  }
+}
+</style>
